550d374fc7488058927570dea4b07be1715e1284
mdyasin111 <mdyasin7636@gmail.com>
2023-03-31 15:54:26 +0600

    bookmarked part design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Blog from './components/Blog/Blog';
+import Bookmarked from './components/Bookmarked/Bookmarked';
 
 const App = () => {
   return (
     <div className="container mx-auto">
-      <Header></Header>
+      <div>
+        <Header></Header>
+      </div>
+      <div className="main row grid grid-cols-1 md:grid-cols-10 mt-6 gap-4">
+        <div className="blogContainer col-span-7">
+          <Blog></Blog>
+        </div>
+        <div className="bookmarked col-span-3">
+          <Bookmarked></Bookmarked>
+        </div>
+      </div>
     </div>
   );
 };
